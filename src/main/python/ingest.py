ce965ccc3842b621585f7bc2143bb9b59fdb5fa7
nking <nichole.king@gmail.com>
2025-09-27 15:51:17 -0700

    implementing more of the ingestion in apache beam

--- src/main/python/ingest.py
+++ src/main/python/ingest.py
@@ -1,5 +1,7 @@
 import apache_beam as beam
 
+from ingest_beam import merge_and_split
+
 import os
 import tensorflow as tf
 from tfx.components.example_gen import base_example_gen_executor
@@ -20,63 +22,23 @@ class ReadMergeAndSplitExecutor(base_example_gen_executor.BaseExampleGenExecutor
     # The keys will depend on your component definition.
     ratings_uri = input_dict['ratings.dat'].uri
     movies_uri = input_dict['movies.dat'].uri
-    users_uri = input_dict['users.dat'].u
+    users_uri = input_dict['users.dat'].uri
+
+    ratings_key_dict = input_dict['ratings_key_dict']
+    movies_key_dict = input_dict['movies_key_dict']
+    users_key_dict = input_dict['users_key_dict']
 
     pipeline = self._MakeBeamPipeline()
 
-    # user_id,movie_id,rating
-    ratings_pc = pipeline | 'ReadRatings' >> beam.io.ReadFromText(ratings_uri) \
-      | 'ParseRatings' >> beam.Map(lambda line: line.split(','))
-
-    # movie_id,genre
-    movies_pc = pipeline | 'ReadMovies' >> beam.io.ReadFromText(movies_uri) \
-      | 'ParseMovies' >> beam.Map(lambda line: line.split(','))
-
-    #UserID::Gender::Age::Occupation::Zip-code
-    users_pc = pipeline | 'ReadUsers' >> beam.io.ReadFromText(users_uri) \
-      | 'ParseUsers' >> beam.Map(lambda line: line.split(','))
-
-    def merge_by_key(l_pc, r_pc, l_key_col, r_key_col):
-      #need unique names for each beam process, so adding a timestamp
-      ts = time.time_ns()
-      l_keyed = l_pc | f'kv_l_{ts}' >> beam.Map(lambda x: (x[l_key_col], x))
-      r_keyed = r_pc | f'kv_r_{ts}' >> beam.Map(lambda x: (x[r_key_col], x))
-
-      # l_keyed | beam.Map(print)
-      # r_keyed | 'beam.Map(print)
-
-      grouped_data = (\
-          {'left': l_keyed, 'right': r_keyed} | \
-          'CoGroupByKey' >> beam.CoGroupByKey())
-      # there are multiple lefts on one line now, and one in right's list
-
-      class left_join_fn(beam.DoFn):
-        def process(self, kv):
-          key, grouped_elements = kv
-          # grouped_elements is a dictionary with keys 'left' and 'right'
-          # both are lists of lists.
-          assert (len(grouped_elements['right']) == 1)
-          for left in grouped_elements['left']:
-            # join grouped_elements['left'] and grouped_elements['right'][0]
-            # merge, reorder etc as wanted
-            row = left.copy()
-            for i, right in enumerate(grouped_elements['right'][0]):
-              if i != r_key_col:
-                row.append(right)
-            yield row
-
-      joined_data = grouped_data | f'left_join_values_{ts}' >> beam.ParDo(left_join_fn())
-      #joined_data |  >> beam.Map(print)
-      return joined_data
-
-    # user_id,movie_id,rating,timestamp,Gender::Age::Occupation::Zip-code
-    ratings_1 = merge_by_key(ratings_pc, users_pc, 0, 1)
-    # user_id,movie_id,rating,Gender::Age::Occupation::Zip-code,genres
-    ratings = merge_by_key(ratings, movies_pc, 1, 0)
+    ratings = merge_and_split(pipeline=pipeline, \
+                              ratings_uri='ratings.dat', movies_uri='movies.dat', \
+                              users_uri='users.dat', \
+                              ratings_key_dict=ratings_key_dict, \
+                              users_key_dict=users_key_dict, \
+                              movies_key_dict=movies_key_dict, \
+                              partition_percents=partition_percents)
 
     #not yet finished
-
-    #TODO: order by timestamp and then split into train and test
 
     #write to output files if wanted
     # The output path is determined by the output_dict.
---
+++ src/main/python/ingest_beam.py
@@ -0,0 +1,82 @@
+import apache_beam as beam
+import time
+
+def merge_and_split(pipeline, ratings_uri, movies_uri, users_uri, \
+  ratings_key_dict, movies_key_dict, users_key_dict, partition_percents):
+  '''
+  :param pipeline:
+  :param ratings_uri:
+  :param movies_uri:
+  :param users_uri:
+  :param ratings_key_dict: for ratings file, a dictionary with key:values
+    being header_column_name:column number
+  :param movies_key_dict: for movies file, a dictionary with key:values being header_column_name:column number
+  :param users_key_dict: for users file, a dictionary with key:values being header_column_name:column number
+  :param partition_percents:
+  :return:
+  '''
+
+  skip = 1
+
+  # user_id,movie_id,rating
+  ratings_pc = pipeline \
+    | 'ReadRatings' >> beam.io.ReadFromText(ratings_uri, skip_header_lines=skip) \
+    | 'ParseRatings' >> beam.Map(lambda line: line.split(','))
+
+  # movie_id,genre
+  movies_pc = pipeline \
+    | 'ReadMovies' >> beam.io.ReadFromText(movies_uri, skip_header_lines=skip) \
+    | 'ParseMovies' >> beam.Map(lambda line: line.split(','))
+
+  # UserID::Gender::Age::Occupation::Zip-code
+  users_pc = pipeline \
+    | 'ReadUsers' >> beam.io.ReadFromText(users_uri, skip_header_lines=skip) \
+    | 'ParseUsers' >> beam.Map(lambda line: line.split(','))
+
+  def merge_by_key(l_pc, r_pc, l_key_col, r_key_col, filter_cols):
+    # need unique names for each beam process, so adding a timestamp
+    ts = time.time_ns()
+    l_keyed = l_pc | f'kv_l_{ts}' >> beam.Map(lambda x: (x[l_key_col], x))
+    r_keyed = r_pc | f'kv_r_{ts}' >> beam.Map(lambda x: (x[r_key_col], x))
+
+    # l_keyed | beam.Map(print)
+    # r_keyed | 'beam.Map(print)
+
+    grouped_data = ({'left': l_keyed, 'right': r_keyed} \
+      | f'group_by_key_{ts}' >> beam.CoGroupByKey())
+    # there are multiple lefts on one line now, and one in right's list
+
+    print(f'filter={filter_cols}')
+
+    class left_join_fn(beam.DoFn):
+      def process(self, kv):
+        key, grouped_elements = kv
+        # grouped_elements is a dictionary with keys 'left' and 'right'
+        # both are lists of lists.
+        assert (len(grouped_elements['right']) == 1)
+        for left in grouped_elements['left']:
+          # join grouped_elements['left'] and grouped_elements['right'][0]
+          # merge, reorder etc as wanted
+          row = left.copy()
+          for i, right in enumerate(grouped_elements['right'][0]):
+            if i != r_key_col and i not in filter_cols:
+              row.append(right)
+          yield row
+
+    joined_data = grouped_data \
+      | f'left_join_values_{ts}' >> beam.ParDo(left_join_fn())
+    return joined_data
+
+  # user_id,movie_id,rating,timestamp,Gender::Age::Occupation::Zip-code
+  ratings_1 = merge_by_key(ratings_pc, users_pc, \
+    ratings_key_dict['user_id'], users_key_dict['user_id'], \
+    filter_cols=[users_key_dict['zipcode']])
+  # user_id,movie_id,rating,Gender::Age::Occupation::Zip-code,genres
+  ratings = merge_by_key(ratings_1, movies_pc, \
+    ratings_key_dict['movie_id'], movies_key_dict['movie_id'],\
+    filter_cols=[])
+
+  print(f'RATINGS type{type(ratings)}')
+  ratings | f'ratings_{time.time_ns()}' >> beam.Map(print)
+
+  return ratings
