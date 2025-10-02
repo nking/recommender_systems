9bc3042a940d017ef354126dd73a279fb5c9dee9
nking <nichole.king@gmail.com>
2025-10-02 13:34:27 -0700

    correcting a map key and missing vars

--- src/main/python/ingest_movie_lens_tfx.py
+++ src/main/python/ingest_movie_lens_tfx.py
@@ -5,15 +5,18 @@ import os
 import absl
 import json
 import pprint
+import bisect
+import hashlib
+import pickle
 
-from typing import Any, Dict, List, Text, Optional
+from typing import Any, Dict, List, Text, Optional, Union
 
 import numpy as np
 import tensorflow as tf
 
 from absl import logging
 
-from tfx.components.example_gen.base_example_gen_executor import BaseExampleGenExecutor, _PartitionFn
+from tfx.components.example_gen.base_example_gen_executor import BaseExampleGenExecutor
 #from tfx.components.example_gen import utils
 from tfx.dsl.components.base import executor_spec, base_component, base_beam_component
 from tfx import types
@@ -108,6 +111,61 @@ class IngestMovieLensExecutorSpec(ComponentSpec):
 # It might be simpler to extend
 # base_executor.BaseExecutor and override the Do method instead of
 # extending BaseExampleGenExecutor as is done here.
+
+def _GeneratePartitionKey(record: Union[tf.train.Example,\
+  tf.train.SequenceExample, bytes, Dict[str, Any]], \
+  split_config: example_gen_pb2.SplitConfig) -> bytes:
+  """Generates key for partition."""
+
+  if not split_config.HasField('partition_feature_name'):
+    if isinstance(record, bytes):
+      return record
+    if isinstance(record, dict):
+      return pickle.dumps(record)
+    return record.SerializeToString(deterministic=True)
+
+  if isinstance(record, tf.train.Example):
+    features = record.features.feature  # pytype: disable=attribute-error
+  elif isinstance(record, tf.train.SequenceExample):
+    features = record.context.feature  # pytype: disable=attribute-error
+  else:
+    raise RuntimeError(
+      'Split by `partition_feature_name` is only supported '
+      'for FORMAT_TF_EXAMPLE and FORMAT_TF_SEQUENCE_EXAMPLE '
+      'payload format.')
+
+  # Use a feature for partitioning the examples.
+  feature_name = split_config.partition_feature_name
+  if feature_name not in features:
+    raise RuntimeError(
+      'Feature name `{}` does not exist.'.format(feature_name))
+  feature = features[feature_name]
+  if not feature.HasField('kind'):
+    raise RuntimeError('Partition feature does not contain any value.')
+  if (not feature.HasField('bytes_list') and
+    not feature.HasField('int64_list')):
+    raise RuntimeError(
+      'Only `bytes_list` and `int64_list` features are '
+      'supported for partition.')
+  return feature.SerializeToString(deterministic=True)
+
+#from https://github.com/tensorflow/tfx/blob/e537507b0c00d45493c50cecd39888092f1b3d79/tfx/components/example_gen/base_example_gen_executor.py#L72
+def _PartitionFn(\
+    record: Union[tf.train.Example, tf.train.SequenceExample, bytes, \
+    Dict[str,Any]], \
+    num_partitions: int, \
+    buckets: List[int], \
+    split_config: example_gen_pb2.SplitConfig,\
+) -> int:
+  """Partition function for the ExampleGen's output splits."""
+  assert num_partitions == len(buckets), 'Partitions do not match bucket number.'
+  partition_str = _GeneratePartitionKey(record, split_config)
+  bucket = int(hashlib.sha256(partition_str).hexdigest(), 16) % buckets[-1]
+  # For example, if buckets is [10,50,80], there will be 3 splits:
+  #   bucket >=0 && < 10, returns 0
+  #   bucket >=10 && < 50, returns 1
+  #   bucket >=50 && < 80, returns 2
+  return bisect.bisect(buckets, bucket)
 
 class IngestMovieLensExecutor(BaseExampleGenExecutor):
   """executor to ingest movie lens data, join, and split into buckets"""
@@ -234,6 +292,9 @@ class IngestMovieLensExecutor(BaseExampleGenExecutor):
 
     ratings_tuple = self.GetInputSourceToExamplePTransform()
 
+    bucket_names = exec_properties['bucket_names']
+    buckets = exec_properties['buckets']
+
     splits = []
     for n, b in zip(bucket_names, buckets):
       #see https://github.com/tensorflow/tfx/blob/e537507b0c00d45493c50cecd39888092f1b3d79/tfx/proto/example_gen.proto#L146
@@ -245,17 +306,14 @@ class IngestMovieLensExecutor(BaseExampleGenExecutor):
       )
     )
 
-    #using _PartitionFn is like using an undocumented method, so might
-    #need to replace it locally if it disappears in future version
-
     #_PartitionFn is from BaseExampleGenExecutor via base_example_gen_executor.py
-    example_splits = (
-      pipeline | 'SplitData' >> beam.Partition(_PartitionFn, len(buckets),
+    example_splits = (\
+      pipeline | 'SplitData' >> beam.Partition(_PartitionFn, len(buckets),\
       buckets, output_config.split_config))
 
     result = {}
     for index, example_split in enumerate(example_splits):
-      result[split_names[index]] = example_split
+      result[bucket_names[index]] = example_split
     return result
 
   def Do(
