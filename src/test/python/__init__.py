45b21a0a2fec3583480b4e19a7746122c3fcc216
nking <nichole.king@gmail.com>
2025-10-02 16:35:29 -0700

    correcting init for test source path

--- src/test/python/__init__.py
+++ src/test/python/__init__.py
@@ -2,4 +2,5 @@ import os
 import sys
 current_dir = os.getcwd()
 two_levels_up = os.path.abspath(os.path.join(current_dir, "../../"))
-sys.path.append(two_levels_up + "/src/python")
+sys.path.append(two_levels_up + "/src/main/python")
+sys.path.append(two_levels_up + "/src/drafts/python")
