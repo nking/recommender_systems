dd7d9ac1d979e670516fbbc948a8cf7d18c9c39f
nking <nichole.king@gmail.com>
2025-10-14 11:20:49 -0700

    reverting to debug

--- src/test/python/ingest_movie_lens_custom_component_test.py
+++ src/test/python/ingest_movie_lens_custom_component_test.py
@@ -40,7 +40,7 @@ import pprint
 import absl
 from absl import logging
 tf.get_logger().propagate = False
-absl.logging.set_verbosity(absl.logging.INFO)
+absl.logging.set_verbosity(absl.logging.DEBUG)
 
 from ingest_movie_lens_custom_component import *
 from movie_lens_utils import *
