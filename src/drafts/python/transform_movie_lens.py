13bdb1a8aa4206ec9b1ace1fb6c85c0d548c8703
nking <nichole.king@gmail.com>
2025-10-14 15:21:50 -0700

    correcting a static hash table lookup method

--- src/drafts/python/transform_movie_lens.py
+++ src/drafts/python/transform_movie_lens.py
@@ -29,10 +29,11 @@ CTZ = pytz.timezone("America/Chicago")
 def _get_raw_feature_spec(schema):
   return schema_utils.schema_as_feature_spec(schema).feature_spec
 
-def create_static_table(var_list):
+def create_static_table(var_list, var_dtype):
   init = tf.lookup.KeyValueTensorInitializer(\
-      keys=tf.constant(var_list), \
-      values=tf.range(len(var_list), dtype=tf.int64))
+      keys=tf.constant(var_list, dtype=var_dtype), \
+      values=tf.range(len(var_list), dtype=tf.int64),\
+      key_dtype=var_dtype, value_dtype=tf.int64)
   return tf.lookup.StaticHashTable(init, default_value=-1)
 
 def preprocessing_fn(inputs):
@@ -53,11 +54,11 @@ def preprocessing_fn(inputs):
   #outputs['rating'] = inputs['rating']/5.0
   labels = tf.cast(inputs['rating'], tf.float32)/5.0
 
-  gender_table = create_static_table(genders)
+  gender_table = create_static_table(genders, var_dtype=tf.string)
   outputs['gender'] = gender_table.lookup(inputs['gender'])
   outputs['gender'] = tf.one_hot( outputs['gender'], depth=len(genders), dtype=tf.int64)
 
-  age_groups_table = create_static_table(age_groups)
+  age_groups_table = create_static_table(age_groups, var_dtype=tf.int64)
   outputs['age'] = age_groups_table.lookup(inputs['age'])
   outputs['age'] = tf.one_hot( outputs['age'], depth=len(age_groups), dtype=tf.int64)
 
@@ -70,7 +71,7 @@ def preprocessing_fn(inputs):
       input = inputs['genres'], pattern="Children's", rewrite="Children")
   #creates a RaggedTensor of strings:
   outputs['genres'] = tf.strings.split(outputs['genres'], "|")
-  genres_table = create_static_table(genres)
+  genres_table = create_static_table(genres, var_dtype=tf.string)
   # creates a RaggedTensor of tf.int64:
   outputs['genres'] = genres_table.lookup(outputs['genres'])
   #the model needs tensors to be same size, so make it dense multithot
