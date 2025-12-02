f78be349dfaa3929257a55072f21cdc6f8f6429e
nichole <nichole@climbwithyourfeet.com>
2025-12-02 12:15:09 -0800

    finished the PTransforms to write tfrecords for retrieval project

--- src/test/python/movie_lens_tfx/WriteRetrievalInputTFRecords.py
+++ src/test/python/movie_lens_tfx/WriteRetrievalInputTFRecords.py
@@ -1,5 +1,6 @@
 import os.path
 import shutil
+import numpy as np
 
 """
 This writes various files that I need for the retrieval project.
@@ -29,7 +30,6 @@ class WriteRetrievalInputTFRecords(tf.test.TestCase):
   same as the ratings_joined columns.  Columns not present in user.dat are filled in with fake numbers.
   
   Create a similar file from movies.data.
-  
   """
   def setUp(self):
     super().setUp()
@@ -67,7 +67,8 @@ class WriteRetrievalInputTFRecords(tf.test.TestCase):
       pipeline1_handle.wait_until_finish()
     if pipeline0_handle is not None:
       pipeline0_handle.wait_until_finish()
-    pipeline_3 = self._left_outer_join_predictions_and_pivot()
+    pipeline3_handle = self._left_outer_join_predictions_and_pivot()
+    pipeline3_handle.wait_until_finish()
     
   def _write_movie_tfrecords(self):
     """
@@ -89,14 +90,17 @@ class WriteRetrievalInputTFRecords(tf.test.TestCase):
     if not self.rewrite_all:
       movie_tf_exists = os.path.exists(self.output_uri1) and bool(os.listdir(self.output_uri1))
       user_tf_exists = os.path.exists(self.output_uri2) and bool(os.listdir(self.output_uri2))
+      print(f'files exist: {movie_tf_exists, user_tf_exists}')
       if movie_tf_exists and user_tf_exists:
         return None
         
     try:
       shutil.rmtree(self.output_uri1)
+      shutil.rmtree(self.output_movie_mm_preds_uri)
     except Exception as ex:
       pass
     os.makedirs(self.output_uri1, exist_ok=True)
+    os.makedirs(self.output_movie_mm_preds_uri, exist_ok=True)
     
     input_column_name_type_list = [('movie_id', int), ('movie_title', str), ('genres', str) ]
     output_column_name_type_list = [('genres', str), ('age', int), ('gender', str),
@@ -121,10 +125,12 @@ class WriteRetrievalInputTFRecords(tf.test.TestCase):
     examples_ser = (examples
       | f"Serialize_{random.randint(0, 1000000000000)}"
       >> beam.Map(lambda x: x.SerializeToString()))
-    
+      
     #write to tfrecords
     #these can be used as inputs for the metadata model
-    examples_ser >> beam.io.tfrecordio.WriteToTFRecord(file_path_prefix=f'{self.output_uri1}/tfrecords', file_name_suffix='.gz')
+    (examples_ser | f'write_movies_fmt_tfrecord{random.randint(0, 1000000000)}'
+      >> beam.io.tfrecordio.WriteToTFRecord(file_path_prefix=f'{self.output_uri1}/tfrecords',
+      file_name_suffix='.gz'))
     
     #calculate predictions from metadata model
     movie_id_and_preds = (examples_ser
@@ -145,9 +151,12 @@ class WriteRetrievalInputTFRecords(tf.test.TestCase):
         WriteRetrievalInputTFRecords.create_example_movie_id_prediction))
     
     #write predictions to tfrecord files
-    movie_id_and_preds_examples >> beam.io.tfrecordio.WriteToTFRecord(
+    (movie_id_and_preds_examples | f"Serialize_movie_pred_{random.randint(0, 1000000000000)}"
+      >> beam.Map(lambda x: x.SerializeToString())
+      | f'write_predictions_tfrecord{random.randint(0, 1000000000)}'
+      >> beam.io.tfrecordio.WriteToTFRecord(
       file_path_prefix=f'{self.output_movie_mm_preds_uri}/tfrecords',
-      file_name_suffix='.gz')
+      file_name_suffix='.gz'))
     
     result1 = pipeline1.run()
     
@@ -184,6 +193,7 @@ class WriteRetrievalInputTFRecords(tf.test.TestCase):
   def _write_train_ratings_pivot_table(self):
     if not self.rewrite_all:
       ratings_pivot_exists = os.path.exists(self.output_uri0) and bool(os.listdir(self.output_uri0))
+      print(f'ratings_pivot_exists files exist: {ratings_pivot_exists}')
       if ratings_pivot_exists:
         return None
       
@@ -263,9 +273,11 @@ class WriteRetrievalInputTFRecords(tf.test.TestCase):
     # each row is a tuple like:
     # (parsed_features['movie_id'] is a tensor like: < tf.Tensor: shape=(1,), dtype = int64, numpy = array([7]),
     # emb is tensor like: tf.Tensor: shape = (1, 32), dtype = float32, numpy = array([[ 0.08...)
-    m_id = row[0].numpy()[0]
-    emb = row[1].numpy()[0]
-    feature_map = {'movie_id': tf.train.Feature(int64_list=tf.train.Int64List(value=[int(m_id)])),
+    m_id = row[0]
+    emb = row[1]
+    #print(f'm_id={m_id}; emb={emb}')
+    feature_map = {
+      'movie_id': tf.train.Feature(int64_list=tf.train.Int64List(value=[m_id])),
       'prediction_mm' : tf.train.Feature(float_list=tf.train.FloatList(value=emb))}
     return tf.train.Example(features=tf.train.Features(feature=feature_map))
   
@@ -306,6 +318,7 @@ class WriteRetrievalInputTFRecords(tf.test.TestCase):
     if not self.rewrite_all:
       pivot_exists = os.path.exists(self.output_pivot_uri) and bool(
         os.listdir(self.output_pivot_uri))
+      print(f'pivot with preds files exist: {pivot_exists}')
       if pivot_exists:
         return None
       
@@ -317,26 +330,87 @@ class WriteRetrievalInputTFRecords(tf.test.TestCase):
     
     pipeline = beam.Pipeline(options=self.pipeline_options)
     
-    pc_preds = (pipeline | f"read_movie_preds_{random.randint(0, 10000000000)}" >>
-      beam.io.ReadFromTFRecord(f'{self.output_movie_mm_preds_uri}/tfrecords*'))
+    # NOTE: beam.CoGroupByKey() needs the keys and values in the groups to be serializable, so
+    # in _ParseSerializedExamples, the tensors are extracted to scalars after parsing to dictionaries
     
-    pc_pivot = (pipeline | f"read_pivot_{random.randint(0, 10000000000)}" >>
-      beam.io.ReadFromTFRecord(f'{self.output_uri0}/tfrecords*'))
+    feature_spec_preds = {
+      'movie_id': tf.io.FixedLenFeature(shape=[], dtype=tf.int64, default_value=None),
+      'prediction_mm': tf.io.FixedLenFeature(shape=[], dtype=tf.float32, default_value=None),
+      }
     
-    #key both sets by movie_id
-    (pc_preds | f"print_read_pred_tfr_{random.randint(0, 10000000000)}" >>
-        beam.Map(lambda x: print(f'mp={x}')))
+    pc_preds = (pipeline | f"read_movie_preds_{random.randint(0, 10000000000)}"
+      >> beam.io.ReadFromTFRecord(file_pattern=f'{self.output_movie_mm_preds_uri}/tfrecords*',
+      compression_type=beam.io.filesystem.CompressionTypes.GZIP)
+      | f"parse_movie_preds_{random.randint(0, 10000000000)}"
+      >>  beam.ParDo(_ParseSerializedExamples(serialize_to_string(feature_spec_preds)))
+      )
     
-    (pc_pivot | f"print_read_pivot_tfr_{random.randint(0, 10000000000)}" >>
-        beam.Map(lambda x: print(f'pivot={x}')))
+    #pc_preds | "print_pc_preds" >> beam.Map(lambda x: print(f'pc_preds={x}'))
     
-    #TODO: finishe here.  key both and write a LeftOuterJoin ParDo
+    feature_spec_pivot = {
+      'movie_id': tf.io.FixedLenFeature(shape=[], dtype=tf.int64, default_value=None),
+      '1': tf.io.FixedLenFeature(shape=[], dtype=tf.int64, default_value=None),
+      '2': tf.io.FixedLenFeature(shape=[], dtype=tf.int64, default_value=None),
+      '3': tf.io.FixedLenFeature(shape=[], dtype=tf.int64, default_value=None),
+      '4': tf.io.FixedLenFeature(shape=[], dtype=tf.int64, default_value=None),
+      '5': tf.io.FixedLenFeature(shape=[], dtype=tf.int64, default_value=None),
+      }
+    
+    pc_pivot = (pipeline | f"read_pivot_{random.randint(0, 10000000000)}"
+      >> beam.io.ReadFromTFRecord(file_pattern=f'{self.output_uri0}/tfrecords*',
+      compression_type=beam.io.filesystem.CompressionTypes.GZIP)
+      | f"parse_pivot_{random.randint(0, 10000000000)}"
+      >>  beam.ParDo(_ParseSerializedExamples(serialize_to_string(feature_spec_pivot)))
+      )
+    
+    #pc_pivot | "print_pc_pivot" >> beam.Map(lambda x: print(f'pc_pivot={x}'))
+
+    l_keyed = pc_preds | f'kv_l_{random.randint(0,1000000000)}' \
+      >> beam.Map(lambda x: (x['movie_id'], x))
+    
+    #l_keyed | "print_l_keyed" >> beam.Map(lambda x: print(f'l_keyed={x}'))
+
+    r_keyed = pc_pivot | f'kv_r_{random.randint(0,1000000000)}' \
+      >> beam.Map(lambda x: (x['movie_id'], x))
+    
+    #r_keyed | "print_r_keyed" >> beam.Map(lambda x: print(f'r_keyed={x}'))
+
+    grouped_data = ({'left': l_keyed, 'right': r_keyed} \
+      | f'group_by_key_preds_pivot_{random.randint(0,1000000000)}' \
+      >> beam.CoGroupByKey())
+    
+    #grouped_data | "print_grouped_data" >> beam.Map(lambda x: print(f'grouped={x}'))
+    
+    try:
+      joined_pivot_data = grouped_data \
+        | f'left_outer_join_preds_pivot_{random.randint(0,1000000000)}' \
+        >> beam.ParDo(_LeftOuterJoinAndSumFn())
+    except Exception as ex:
+      logging.error(f"ERROR {ex}")
+      raise ex
+    
+    #joined_pivot_data | "print_joined_pivot" >> beam.Map(lambda x: print(f'joined_pivot={x}'))
+    
+    column_name_type_dict = {'movie_id': int, 'prediction_mm': float, '1': int, '2': int,
+      '3': int, '4': int, '5': int}
+    
+    (joined_pivot_data | f'joined_pivot_ToTFExample_{random.randint(0, 1000000000000)}'
+      >> beam.Map(WriteRetrievalInputTFRecords.create_pivot_example,column_name_type_dict)
+      | f"Serialize_joined_pivot_{random.randint(0, 1000000000000)}"
+      >> beam.Map(lambda x: x.SerializeToString())
+      | f"write_joined_pivot_to_tfrecord_{random.randint(0, 1000000000000)}"
+      >> beam.io.tfrecordio.WriteToTFRecord(
+      file_path_prefix=f'{self.output_pivot_uri}/tfrecords', file_name_suffix='.gz'))
+    
+    result = pipeline.run()
+    
+    return result
 
 class _CalcMetadataModelPredictions(beam.DoFn):
   def __init__(self, saved_model_path:str, schema_path:str):
     self.saved_model_path = saved_model_path
     self.schema_path = schema_path
-    self.candidate_model = None #pickling error, so delay until setup
+    self.metadata_model = None #pickling error, so delay until setup
     self.feature_spec = None
     self.INPUT_KEY = None
   
@@ -346,18 +420,73 @@ class _CalcMetadataModelPredictions(beam.DoFn):
     This is where you load heavy, non-serializable resources.
     """
     loaded_user_movie_model = tf.saved_model.load(self.saved_model_path)
-    self.candidate_model = loaded_user_movie_model.signatures["serving_candidate"]
+    self.metadata_model = loaded_user_movie_model.signatures["serving_default"]
     raw_schema = tfx.utils.parse_pbtxt_file(self.schema_path, schema_pb2.Schema())
     self.feature_spec = schema_utils.schema_as_feature_spec(raw_schema).feature_spec
-    self.INPUT_KEY = list(self.candidate_model.structured_input_signature[1].keys())[0]
-    
+    self.INPUT_KEY = list(self.metadata_model.structured_input_signature[1].keys())[0]
+
   def process(self, example_ser):
     parsed_features = tf.io.parse_single_example(example_ser, self.feature_spec)
     #wrap example_ser in a list because it expects a batch of inputs
-    emb = self.candidate_model(**{self.INPUT_KEY: [example_ser]})['outputs']
+    pred = self.metadata_model(**{self.INPUT_KEY: [example_ser]})['outputs']
     #parsed_features['movie_id'] is a tensor like: < tf.Tensor: shape=(1,), dtype = int64, numpy = array([7])
     #emb is tensor like: tf.Tensor: shape = (1, 32), dtype = float32, numpy = array([[ 0.08...
-    yield (parsed_features['movie_id'], emb)
+    m_id = int(parsed_features['movie_id'].numpy()[0])
+    pred = float(pred.numpy()[0])
+    yield (m_id, pred)
+
+
+class _LeftOuterJoinAndSumFn(beam.DoFn):
+  def setup(self):
+    self.empty_right = {'1': 0, '2': 0, '3': 0,'4': 0,  '5': 0}
+    
+  def process(self, kv):
+    """
+    perform left outter join and sum over each of the right entries
+    """
+    movie_id, grouped_elements = kv
+    
+    #there should only be 1 left
+    left = grouped_elements['left']
+    if len(left) != 1:
+      raise ValueError(f"left should contain 1 dictionary.  left={left}")
+    left = left[0]
+    
+    right = grouped_elements['right']
+    row = left.copy()
+    row.update(self.empty_right)
+    
+    if len(right) == 0:
+      yield row
+    else:
+      for right_row in right:
+         for key, value in right_row.items():
+           row[key] += value
+    yield row
+
+class _ParseSerializedExamples(beam.DoFn):
+  """
+  converts string serialized examples into dictionaries of tensors, then converts
+  the dictionary to dictionary of scalars.
+  """
+  def __init__(self, feature_spec_ser: str):
+    self.feature_spec_ser = feature_spec_ser
+    self.feature_spec = None
+  
+  def setup(self):
+    self.feature_spec = deserialize(self.feature_spec_ser)
+  
+  def process(self, example_ser):
+    tensor_dict = tf.io.parse_single_example(example_ser, self.feature_spec)
+    # beam.CoGroupByKey cannot handle tensors (unless there is some way with a type hint)
+    # so extract scalars from tensors
+    for key in tensor_dict:
+      val = tensor_dict[key].numpy()
+      if isinstance(val, np.floating):
+        tensor_dict[key] = float(val)
+      else:
+        tensor_dict[key] = int(val)
+    yield tensor_dict
     
 class _PivotCombineFn(beam.CombineFn):
   def create_accumulator(self):
