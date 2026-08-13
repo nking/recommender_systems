a5c0e892f9e3e46f159c4485bbce2df44af6a5f3
nichole <nichole@climbwithyourfeet.com>
2026-08-13 07:29:22 -0700

    changing layer_sizes to [24]

--- src/main/python/movie_lens_tfx/tune_train_movie_lens.py
+++ src/main/python/movie_lens_tfx/tune_train_movie_lens.py
@@ -1676,7 +1676,7 @@ def get_default_hyperparameters(custom_config) -> keras_tuner.HyperParameters:
   #the last layer in layer_sizes is the query and candidate embedding models' output dimensions-1
   #hp.Choice("layer_sizes", values=[json.dumps([16])], default=json.dumps([16]))
   #hp.Fixed("layer_sizes", value=json.dumps([32]))
-  hp.Fixed("layer_sizes", value=json.dumps([32]))
+  hp.Fixed("layer_sizes", value=json.dumps([24])) # 16 too low, 64 too high.   32 good.  24 good?
   # ahmos for "age", "hr_wk", "month", "occupation", "gender"
   hp.Fixed("feature_acronym", custom_config.get("feature_acronym", "h"))
   hp.Fixed("incl_genres", custom_config["incl_genres"])
